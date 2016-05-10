807354c216b9b7c79456802d5aac89acb79788ce
Bruce Ediger <bediger@stratigery.com>
2016-05-09 18:35:57 -0600

    Clean up a bit for readability.

--- Checker.php
+++ Checker.php
@@ -10,13 +10,15 @@ $hsv->reasons = TRUE;
 $ycbcr = new NudeDetector(null, 'YCbCr');
 $ycbcr->reasons = TRUE;
 foreach ($argv as $idx => $arg) {
+
 	if ($idx == 0) continue;
+
 	$hsv->set_file_name($arg);
 	$y = $hsv->is_nude();
-    #$frac = (float)$hsv->$background_pixel_count/(float)$skin_pixel_count;
 	echo $arg . "\tHSV\t";
 	if ($y) echo "NUDITY!\n";
 	else echo "Not nude.\n";
+
 	$ycbcr->set_file_name($arg);
 	$y = $ycbcr->is_nude();
 	echo $arg . "\tYCbCr\t";
--- skin_maps.php
+++ skin_maps.php
@@ -8,16 +8,12 @@ if ($argc == 1)
 $input_file = $argv[1];
 $prefix = $argv[2];
 
-$islamic = new NudeDetector($input_file, 'islamic');
 $hsv = new NudeDetector($input_file, 'HSV');
 $ycbcr = new NudeDetector($input_file, 'YCbCr');
 
-$islamic->map_skin_pixels();
 $hsv->map_skin_pixels();
 $ycbcr->map_skin_pixels();
 
-$img = $islamic->skin_map;
-imagegif($img, $prefix .'islamic.gif');
 $img = $hsv->skin_map;
 imagegif($img, $prefix .'HSV.gif');
 $img = $ycbcr->skin_map;
